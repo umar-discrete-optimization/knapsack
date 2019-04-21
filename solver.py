e88983cda855c8d00b073c0af93bdce039e78b62
Umar Mohammed <umar@teacherapp.co.uk>
2019-04-21 16:25:59 +0100

    refactor greedy algorithm
    #1

--- solver.py
+++ solver.py
@@ -1,43 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from scenario import Scenario
+from greedySolver import GreedySolver
+
 from collections import namedtuple
 Item = namedtuple("Item", ['index', 'value', 'weight'])
 
 def solve_it(input_data):
-    # Modify this code to run your optimization algorithm
-
-    # parse the input
-    lines = input_data.split('\n')
-
-    firstLine = lines[0].split()
-    item_count = int(firstLine[0])
-    capacity = int(firstLine[1])
-
-    items = []
-
-    for i in range(1, item_count+1):
-        line = lines[i]
-        parts = line.split()
-        items.append(Item(i-1, int(parts[0]), int(parts[1])))
-
-    # a trivial greedy algorithm for filling the knapsack
-    # it takes items in-order until the knapsack is full
-    value = 0
-    weight = 0
-    taken = [0]*len(items)
-
-    for item in items:
-        if weight + item.weight <= capacity:
-            taken[item.index] = 1
-            value += item.value
-            weight += item.weight
-    
-    # prepare the solution in the specified output format
-    output_data = str(value) + ' ' + str(0) + '\n'
-    output_data += ' '.join(map(str, taken))
-    return output_data
-
+    solver = GreedySolver(input_data)
+    solver.solve()
+    return solver.output_solution()
 
 if __name__ == '__main__':
     import sys
